2ec94904cb5497f684bcf52ddf1ca044129b4216
Koj2584 <73990215+Koj2584@users.noreply.github.com>
2022-12-12 09:32:40 +0100

    upload

---
+++ OOP - DU4/OOP - DU4/NakladniAuto.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP___DU4
+{
+    class NakladniAuto
+    {
+        protected string spz;
+        protected short nosnost;
+        protected short hmotnostNakladu = 0;
+        protected short HmotnostNakladu
+        {
+            get
+            {
+                return hmotnostNakladu;
+            }
+
+        }
+
+        public NakladniAuto(string spz, short nosnost)
+        {
+            this.spz = spz;
+            this.nosnost = nosnost;
+        }
+
+        public void NalozNaklad(short vaha)
+        {
+            if (vaha + hmotnostNakladu > nosnost)
+            {
+                hmotnostNakladu = nosnost;
+                MessageBox.Show("Nenaložilo se " + (vaha - nosnost) + " tun.");
+            }
+            else
+                hmotnostNakladu += vaha;
+        }
+
+        public void VylozNaklad(short vaha)
+        {
+            if (vaha > hmotnostNakladu)
+            {
+                hmotnostNakladu = 0;
+                MessageBox.Show("Chybějící náklad  " + (vaha - hmotnostNakladu) + " tun.");
+            }
+            else
+                hmotnostNakladu -= vaha;
+        }
+
+
+    }
+}
